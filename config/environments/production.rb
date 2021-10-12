bf732280e289c56a2ef845d26a9ea1d010e1bb29
JAMChannel <pon244tt@gmail.com>
2021-10-13 02:35:46 +0900

    mailer3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
